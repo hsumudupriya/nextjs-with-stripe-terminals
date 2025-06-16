33ca1781dc76530ef165afd009fffa85ca1bdb42
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2025-06-16 23:05:08 +0530

    sequelize.ts file is updated for production

--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -5,15 +5,32 @@
 
 import { Dialect, Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME!,
-    process.env.DB_USER!,
-    process.env.DB_PASS!,
-    {
-        host: process.env.DB_HOST!,
+let sequelize: Sequelize;
+
+if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    // In production, use the DATABASE_URL
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: process.env.DB_DIALECT! as Dialect,
-        port: Number(process.env.DB_PORT) || 3306,
-    }
-);
+        // It's good practice to configure connection pooling for production
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+    });
+} else {
+    // In development, use individual environment variables from .env.local
+    sequelize = new Sequelize(
+        process.env.DB_NAME!,
+        process.env.DB_USER!,
+        process.env.DB_PASS!,
+        {
+            host: process.env.DB_HOST!,
+            dialect: process.env.DB_DIALECT! as Dialect,
+            port: Number(process.env.DB_PORT) || 3306,
+        }
+    );
+}
 
 export default sequelize;
